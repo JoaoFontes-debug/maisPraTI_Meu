de7c10189a285574d57a76e19249c5e8a6838cd6
João Fontes <j.henry.kings@gmail.com>
2024-07-09 23:22:17 -0300

    Menu de abas. Estilo e estrutura finalizados

---
+++ Modulo04/aula03/menuDePaginas/menuPaginas/src/components/Body.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import body from './Body.module.css'
+
+function Body(){
+    return (
+        <div className={body}>
+
+        </div>
+    )
+}
+
+export default Body; 
--- Modulo04/aula03/menuDePaginas/menuPaginas/src/components/Header.jsx
+++ Modulo04/aula03/menuDePaginas/menuPaginas/src/components/Header.jsx
@@ -1,16 +1,30 @@
 import React from "react";
 import logo from './images/logo.png';
-import './App.css'
+import style from './Header.module.css';
 
 function Header(){
     return(
-        <div className="Header" >
-            <img src={logo} alt="logo"  className="logo"/>
-            <h1>React.js</h1>
-            <p>i.e., using the React library for rendering the UI</p>
+        <div className={style.header}>
+            <div className={style.conteudoHeader}>
+                <img src={logo} alt="logo"  className={style.logo}/>
+                <h1>React.js</h1>
+                <p>i.e., using the React library for rendering the UI</p>
+            </div>
+                
+            <div className={style.media}>
+                <ul className={style.botoes}>
+                    <li><a href="">Why React?</a></li>
+                    <li><a href="">Core Features</a></li>
+                    <li><a href="">Related Resources</a></li>
+                    <li><a href="">React vs JS</a></li>
+                </ul>
+                <div className={style.painel}>
+
+                </div>
+            </div>
 
         </div>
-    )
+    )   
 }
 
 export default Header;
